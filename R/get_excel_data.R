c08ca9bfb1c50c5cb885412dd1837c4da07d876a
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2020-06-07 14:41:47 +0100

    Update get_excel_data.R

--- R/get_excel_data.R
+++ R/get_excel_data.R
@@ -28,103 +28,108 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
 
   temp.list.tilde <- which(!grepl("\\~", temp.list)) # Mark which rows to keep.
   temp.list <- temp.list[temp.list.tilde]
+
+
   temp.list.path <- temp.list
+
+  # Mollusc_data <- read_excel("inst/dev/utils.template.network.inst.dev/data-raw/Mollusc data.xlsx")
+
+
 
   # If the number of excel files is greater than 0
   if(length(temp.list > 0)) {
 
-        for(i in 1:length(temp.list)) {
-          my.file <- temp.list.path[[i]]
+    for(i in 1:length(temp.list)) {
+      my.file <- temp.list.path[[i]]
 
-          ### check for multiple sheets ####
-          temp.excel.sheets <-
-             readxl::excel_sheets(my.file )
-            #colnames(temp.excel.sheets) <- c("sheet.name")
-            temp.excel.sheets[] <- lapply(temp.excel.sheets, as.character)
+      ### check for multiple sheets ####
+      temp.excel.sheets <-
+        readxl::excel_sheets(my.file )
+      #colnames(temp.excel.sheets) <- c("sheet.name")
+      temp.excel.sheets[] <- lapply(temp.excel.sheets, as.character)
 
-          for(sheet.index in 1:length(temp.excel.sheets) ) {
-             sheet.name <- temp.excel.sheets[[sheet.index]]
-              # Read the files but set all columns to text
-              temp.my.data.excel <- as.data.frame(readxl::read_excel(my.file,
-                                                  col_types = "text",
-                                                  sheet = sheet.name)
-                                                    )
+      for(sheet.index in 1:length(temp.excel.sheets) ) {
+        sheet.name <- temp.excel.sheets[[sheet.index]]
+        # Read the files but set all columns to text
+        temp.my.data.excel <- readxl::read_excel(my.file, col_types = "text", sheet = sheet.name) %>%
+          as.data.frame()
 
-             #  # Try and read clean dates
-             #  my.clean.dates <- as.data.frame(SurveyFiles::Clean.dates(
-             #                              temp.my.data.excel$Date)
-             #                              )
-             #  # Which rows are clean
-             #  rows.with.dates <- grepl("\\d{4}-\\d{2}-\\d{2}",
-             #                              my.clean.dates$YYMMDD )
-             #
-             # # Append the clean cloumns
-             #  my.clean.dates$Valid.dates <- rows.with.dates
-             # temp.my.data.excel <- cbind(temp.my.data.excel,my.clean.dates )
-    #
-    #         # Now to add location columns
-    #         colnames(temp.my.data.excel)[which(colnames(
-    #           temp.my.data.excel) == "Latitude..WGS84.")] <- c("Lat")
-    #         colnames(temp.my.data.excel)[which(colnames(
-    #           temp.my.data.excel) == "Longitude..WGS84.")] <- c("Long")
-    #
-    #         # Create a time stamp in POSIX format
-            # temp.time.split <- stringr::str_split(temp.my.data.excel$Timestamp, " ")
-            # temp.time.split.df <- matrix(
-            #   unlist(temp.time.split), ncol=2, byrow=TRUE) %>% as.data.frame()
-            # colnames(temp.time.split.df) <- c("Date", "Time")
-            # temp.my.data.excel$Time <- temp.time.split.df$Time
-            # temp.my.data.excel$Date <- temp.time.split.df$Date
-            nice.file.name <- make.names(my.file)
-             nice.sheet.name <- make.names(sheet.name)
-              # Add the file as source
-              temp.my.data.excel$source <- paste(nice.file.name,"Sheet",nice.sheet.name, sep = ".")
-          ####################Construct a neat wide format################
+        # Try and read clean dates
+        my.clean.dates <- utils.template.network::Clean.dates(
+          temp.my.data.excel$Date) %>%
+          as.data.frame()
+        # Which rows are clean
+        rows.with.dates <- grepl("\\d{4}-\\d{2}-\\d{2}",
+                                 my.clean.dates$YYMMDD )
+
+        # Append the clean cloumns
+        my.clean.dates$Valid.dates <- rows.with.dates
+        temp.my.data.excel <- cbind(temp.my.data.excel,my.clean.dates )
+        #
+        #         # Now to add location columns
+        #         colnames(temp.my.data.excel)[which(colnames(
+        #           temp.my.data.excel) == "Latitude..WGS84.")] <- c("Lat")
+        #         colnames(temp.my.data.excel)[which(colnames(
+        #           temp.my.data.excel) == "Longitude..WGS84.")] <- c("Long")
+        #
+        #         # Create a time stamp in POSIX format
+        # temp.time.split <- stringr::str_split(temp.my.data.excel$Timestamp, " ")
+        # temp.time.split.df <- matrix(
+        #   unlist(temp.time.split), ncol=2, byrow=TRUE) %>% as.data.frame()
+        # colnames(temp.time.split.df) <- c("Date", "Time")
+        # temp.my.data.excel$Time <- temp.time.split.df$Time
+        # temp.my.data.excel$Date <- temp.time.split.df$Date
+        nice.file.name <- make.names(my.file)
+        nice.sheet.name <- make.names(sheet.name)
+        # Add the file as source
+        temp.my.data.excel$source <- paste(nice.file.name,"Sheet",nice.sheet.name, sep = ".")
 
 
-            ################ Get the columns ##############
-            # Intialise
-            my.file.type <- c("unknown")
-            test.temp.my.file <- temp.my.data.excel
 
-            # Length of target list
-            length.target <- length(my.columns[[1]])
+        ################ Get the columns ##############
+        # Intialise
+        my.file.type <- c("unknown")
+        test.temp.my.file <- temp.my.data.excel
 
-                # Work through the list
-                for(i in 1:length(my.columns)) {
-                  working.list <- my.columns[[i]]
-                  length.intersect <-length( intersect(colnames(test.temp.my.file),
-                                                       working.list))
+        # Length of target list
+        length.target <- length(my.columns[[1]])
 
-                      # Create an index we can use for transformations
-                  # if( length.intersect == length.target && my.file.type == "unknown" ) {
-                  #   my.file.type <- paste("Type ",i, sep ="")
-                  #   test.my.file.cols <- test.temp.my.file[,working.list]
-                  #   # Rename the columns as per one
-                  #   colnames(test.my.file.cols) <- my.columns[[1]]
-                  #
-                  #   test.temp.my.file <- test.my.file.cols
-                  #
-                  #   # Now make a record of the transformation in the source column
-                  #   test.temp.my.file$source <- paste(test.temp.my.file$source,
-                  #                                     my.file.type, sep = "," )
-                  # } # end add source index
-                  ############
+        # Work through the list
+        for(i in 1:length(my.columns)) {
+          working.list <- my.columns[[i]]
+          length.intersect <-length( intersect(colnames(test.temp.my.file),
+                                               working.list))
 
-                  # Now test if the data OK
-                  save.directory <- "data-NA"
+          # Create an index we can use for transformations
+          if( length.intersect == length.target && my.file.type == "unknown" ) {
+            my.file.type <- paste("Type ",i, sep ="")
+            test.my.file.cols <- test.temp.my.file[,working.list]
+            # Rename the columns as per one
+            colnames(test.my.file.cols) <- my.columns[[1]]
 
-                  if(!my.file.type == "unknown") {save.directory <- "data-clean"}
+            test.temp.my.file <- test.my.file.cols
 
-                  # We will now save each file in data-clean or data-NA
-                  temp.nice.name <- paste(nice.file.name,nice.sheet.name, sep = "." )
-                  temp.nice.name.rds <- paste(temp.nice.name, ".rds", sep="")
-                  temp.data.to.save.path <- fs::path(save.directory, temp.nice.name.rds)
+            # Now make a record of the transformation in the source column
+            test.temp.my.file$source <- paste(test.temp.my.file$source,
+                                              my.file.type, sep = "," )
+          } # end add source index
+          ############
 
-                  saveRDS(test.temp.my.file, file = temp.data.to.save.path)
+          # Now test if the data OK
+          save.directory <- "data-NA"
 
-                } # End if
-            ###############
+          if(!my.file.type == "unknown") {save.directory <- "data-clean"}
+
+          # We will now save each file in data-clean or data-NA
+          temp.nice.name <- paste(nice.file.name,nice.sheet.name, sep = "." )
+          temp.nice.name.rds <- paste(temp.nice.name, ".rds", sep="")
+          temp.data.to.save.path <- fs::path(save.directory, temp.nice.name.rds)
+
+          saveRDS(test.temp.my.file, file = temp.data.to.save.path)
+
+        } # End if
+        ###############
+
 
 
 
