b00fbe537a0f22938d75b5fce9e9c87e57069b54
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2020-06-09 10:52:31 +0100

    Update get_excel_data.R

--- R/get_excel_data.R
+++ R/get_excel_data.R
@@ -41,7 +41,7 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
 
   # If the number of excel files is greater than 0
   if(length(temp.list > 0)) {
-
+    # For length of temp.list
     for(i in 1:length(temp.list)) {
       my.file <- temp.list.path[[i]]
 
@@ -68,20 +68,7 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
         # Append the clean cloumns
         my.clean.dates$Valid.dates <- rows.with.dates
         temp.my.data.excel <- cbind(temp.my.data.excel,my.clean.dates )
-        #
-        #         # Now to add location columns
-        #         colnames(temp.my.data.excel)[which(colnames(
-        #           temp.my.data.excel) == "Latitude..WGS84.")] <- c("Lat")
-        #         colnames(temp.my.data.excel)[which(colnames(
-        #           temp.my.data.excel) == "Longitude..WGS84.")] <- c("Long")
-        #
-        #         # Create a time stamp in POSIX format
-        # temp.time.split <- stringr::str_split(temp.my.data.excel$Timestamp, " ")
-        # temp.time.split.df <- matrix(
-        #   unlist(temp.time.split), ncol=2, byrow=TRUE) %>% as.data.frame()
-        # colnames(temp.time.split.df) <- c("Date", "Time")
-        # temp.my.data.excel$Time <- temp.time.split.df$Time
-        # temp.my.data.excel$Date <- temp.time.split.df$Date
+
         nice.file.name <- make.names(my.file)
         nice.sheet.name <- make.names(sheet.name)
         # Add the file as source
@@ -94,14 +81,18 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
         my.file.type <- c("unknown")
         test.temp.my.file <- temp.my.data.excel
 
+        # Now test if the data OK
+        save.directory <- "data-NA"
+
         # Length of target list
         length.target <- length(my.columns[[1]])
 
-        # Work through the list
+        # For length of columns
         for(i in 1:length(my.columns)) {
           working.list <- my.columns[[i]]
           length.intersect <-length( intersect(colnames(test.temp.my.file),
                                                working.list))
+
 
           # Create an index we can use for transformations
           if( length.intersect == length.target && my.file.type == "unknown" ) {
@@ -115,35 +106,27 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
             # Now make a record of the transformation in the source column
             test.temp.my.file$source <- paste(test.temp.my.file$source,
                                               my.file.type, sep = "," )
+            # And set the source directory to data-sl
+            save.directory <- "data-sl"
           } # end add source index
           ############
 
-          # Now test if the data OK
-          save.directory <- "data-NA"
-
-          if(!my.file.type == "unknown") {save.directory <- "data-sl"}
-
-          # We will now save each file in data-clean or data-NA
-          temp.nice.name <- paste(nice.file.name,nice.sheet.name, sep = "." )
-          temp.nice.name.rds <- paste(temp.nice.name, ".rds", sep="")
-          temp.data.to.save.path <- fs::path(save.directory, temp.nice.name.rds)
-
-          saveRDS(test.temp.my.file, file = temp.data.to.save.path)
-
-        } # End if
-        ###############
 
 
 
 
+        } # End for length of columns
+        #         if(!my.file.type == "unknown") {save.directory <- "data-sl"}
 
+        # We will now save each file in data-ss or data-NA
+        temp.nice.name <- paste(nice.file.name,nice.sheet.name, sep = "." )
+        temp.nice.name.rds <- paste(temp.nice.name, ".rds", sep="")
+        temp.data.to.save.path <- fs::path(save.directory, temp.nice.name.rds)
 
-
-
-
+        saveRDS(test.temp.my.file, file = temp.data.to.save.path)
+             ###############
                 } # end sheets
-                } # end check columns
-              # Now add any specific the transformations
+                } # end For length of temp.list
 
 
 
@@ -154,7 +137,10 @@ get_excel_data <- function(data_raw_directory="data-source", columns_list) {
 
 
 
-  } # End if files to transform
+
+  } # End If the number of excel files is greater than 0
+
+
 
   number.of.files <- length(temp.list)
   return(number.of.files)
