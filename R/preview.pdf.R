54575c4b2e3f4d8f3f1c36a2718082393928da3e
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2021-05-27 22:45:12 +0100

    Update and add function

---
+++ R/preview.pdf.R
@@ -0,0 +1,26 @@
+
+#' Preview.pdf
+#'
+#' Generates a preview of the currently active bookdown chapter
+#'
+#'
+#'
+#' @return Nothing returned
+#' @export preview.pdf
+#'
+#'
+preview.pdf <- function() {
+# Preview
+# Current source active file directory
+current.pane <- dirname(rstudioapi::getSourceEditorContext()$path)
+#current.pane <- getwd()
+#where.am.i <- rstudioapi::getSourceEditorContext()
+# Find the base directory
+directory.base.name <- basename(current.pane)
+current.chapter.Rmd <- fs::path(directory.base.name, ext = "Rmd")
+output.pdf <- fs::path(directory.base.name, ext = "pdf")
+
+setwd(current.pane)
+bookdown::preview_chapter(current.chapter.Rmd,output_file = output.pdf)
+setwd(rprojroot::find_rstudio_root_file())
+}
