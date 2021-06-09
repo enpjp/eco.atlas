3f870a43ac5850f1ea63d1bf7fdfe2c7ce7382c3
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2021-06-09 22:08:21 +0100

    updated function to compress jpegs

--- R/preview.pdf.R
+++ R/preview.pdf.R
@@ -15,14 +15,47 @@ preview.pdf <- function() {
 # Current source active file directory
 current.pane <- dirname(rstudioapi::getSourceEditorContext()$path)
 #current.pane <- getwd()
+setwd(current.pane)
 #where.am.i <- rstudioapi::getSourceEditorContext()
 # Find the base directory
 directory.base.name <- basename(current.pane)
 current.chapter.Rmd <- fs::path(directory.base.name, ext = "Rmd")
+
+# Make sure that jpegs are the correct size.
+
+my.jpeg.files <- list.files(
+  current.pane,
+  pattern = ".jp?g$", # Make a suitable filter.
+  full.names = TRUE,
+  recursive = TRUE)
+
+my.jpeg.info  <-  magick::image_info( magick::image_read(my.jpeg.files)) %>% as.data.frame()
+
+jpeg.rows.to.change <- which(my.jpeg.info$height > 1000 )
+
+jpeg.files.to.change <- my.jpeg.files[jpeg.rows.to.change]
+
+imagemagick.files <- magick::image_read(jpeg.files.to.change)
+
+#imagemagick.files.resized <- image_resize(imagemagick.files, geometry_size_pixels(height = 1000))
+
+imagemagick.files.resized <- magick::image_resize(imagemagick.files, "x1000")
+
+# Image write does not behave correctly as a vector
+if(length(jpeg.files.to.change) > 0) {
+  for (my.new.image in 1:length(jpeg.files.to.change)) {
+
+    magick::image_write(
+      imagemagick.files.resized[my.new.image],
+      path = jpeg.files.to.change[my.new.image]
+    )
+  }
+
+}
+
+
+# Build the PDF
 output.pdf <- fs::path(directory.base.name, ext = "pdf")
-
-
-setwd(current.pane)
 bookdown::preview_chapter(current.chapter.Rmd,output_file = output.pdf)
 # Now compact the pdf
 tools::compactPDF(output.pdf,qpdf = "qpdf", gs_cmd = "gs", gs_quality = "ebook")
