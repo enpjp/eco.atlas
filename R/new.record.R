5b99c2795a87d6823e0ced6387f9b94f0a5569b1
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2021-08-30 13:50:42 +0100

    Updated functionality

--- R/new.record.R
+++ R/new.record.R
@@ -2,10 +2,15 @@
 
 #' new.record
 #'
+#'
+#'@param index.reference A unique record identifier.
+#'@param Order Record order.
+#'
+#'
 #' @return Nothing returned
 #' @export new.record
 #'
-new.record <- function(index.reference) {
+new.record <- function(index.reference,Order = "Lepidoptera") {
 
   my.working.project <- rstudioapi::getActiveProject()
 
@@ -15,12 +20,14 @@ new.record <- function(index.reference) {
   # Now create an images directory
   images.dir <- fs::path(my.working.directory,"images")
 
+  # Path to inst
+ data_model <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
   # Now create the template file
   usethis::with_project( my.working.project,
-                         usethis::use_template("rmarkdown-template.Rmd",
-                                               save_as = fs::path(template.path.name,
-                                                                  "skeleton", ext = "Rmd"),
-                                               package = this.package.name
+                         usethis::use_template(data_model,
+                                               save_as = fs::path(my.working.directory,
+                                                                  index.reference, ext = "Rmd"),
+                                               reference = index.reference
                          )
                          # End use with project
   )
