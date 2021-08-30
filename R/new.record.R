720f98ffa3d239823bcfa83f50d7206f3b1c5e03
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2021-08-30 14:18:15 +0100

    Update new.record.R

--- R/new.record.R
+++ R/new.record.R
@@ -10,27 +10,45 @@
 #' @return Nothing returned
 #' @export new.record
 #'
-new.record <- function(index.reference,Order = "Lepidoptera") {
+new.record <- function(index.reference, Order = "Lepidoptera") {
+
+  my.lists <- list(
+    index.reference = index.reference,
+    order = Order
+
+    )
 
   my.working.project <- rstudioapi::getActiveProject()
 
   my.working.directory <- fs::path(getwd())
 
   dir.create(index.reference)
+
   # Now create an images directory
-  images.dir <- fs::path(my.working.directory,"images")
+  images.dir <- fs::path(my.working.directory,index.reference, "images")
+  dir.create(index.reference)
 
   # Path to inst
- data_model <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
+ data.model.path <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
   # Now create the template file
-  usethis::with_project( my.working.project,
-                         usethis::use_template(data_model,
-                                               save_as = fs::path(my.working.directory,
-                                                                  index.reference, ext = "Rmd"),
-                                               reference = index.reference
-                         )
-                         # End use with project
-  )
+ my.template <- readr::read_file(data.model.path) %>%   as.character()
+
+ my.rendered.output <-
+   whisker::whisker.render(my.template, data = my.lists, strict = FALSE)
+ # Write the file
+ rendered.path <- fs::path(my.working.directory,
+                           index.reference,
+                           index.reference, ext = "Rmd")
+ readr::write_file(my.rendered.output, rendered.path)
+
+  # usethis::with_project( my.working.project,
+  #                        usethis::use_template(data_model,
+  #                                              save_as = fs::path(my.working.directory,
+  #                                                                 index.reference, ext = "Rmd"),
+  #                                              index.reference = index.reference
+  #                        )
+  #                        # End use with project
+  # )
 
 
 }
