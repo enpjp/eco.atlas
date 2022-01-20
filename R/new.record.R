0e0f4a79184681648e4a0cef6f1ee59c800f83fa
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2022-01-20 20:21:45 +0000

    added external template support

--- R/new.record.R
+++ R/new.record.R
@@ -4,65 +4,19 @@
 #'
 #'
 #'@param index.reference A unique record identifier.
-#'@param record.date ISO Date.
-#'@param place.name Place name.
-#'@param grid.ref Grid ref.
-#'@param recorder.name Recorder Name.
-#'@param record.notes Record notes.
-#'@param method Method of recording.
-#'@param determiner The determiner.
-#'@param order Taxon order.
-#'@param family Family.
-#'@param sub.family Sub.family often used for Lepidoptera.
-#'@param genus Genus without species name.
-#'@param taxon Genus and species name.
-#'@param log.number Log number for putting species in taxonomic order.
-#'@param Bradley Bradley Fletcher Log number.
-#'@param common.name Vernacular name.
-#'@param gender Male or Female?
+#'@param datum.attributes Named list of attributes to use in template
+#'@param data.model.path Path to external template
+
 #'
 #' @return Nothing returned
 #' @export new.record
 #'
 new.record <- function(index.reference,
-                       record.date = "Date",
-                       place.name = "Place Name",
-                       grid.ref = "Grid Reference",
-                       recorder.name = "Recorder Name",
-                       record.notes = "Record Notes",
-                       method = "Method",
-                       determiner = "Determiner Name",
-                       order = "Lepidoptera",
-                       family = "Family",
-                       sub.family = "Sub-Family",
-                       genus = "Genus",
-                       taxon = "Taxon",
-                       log.number = "Classification Log Number",
-                       Bradley = "Bradley Fletcher Number",
-                       common.name = "Common Name",
-                       gender = "Male or Female"
+                       datum.attributes,
+                       data.model.path = NULL
                        ) {
 
-  my.lists <- list(
-    index.reference = index.reference,
-    record.date = record.date,
-    place.name = place.name,
-    grid.ref = grid.ref,
-    recorder.name = recorder.name,
-    record.notes = record.notes,
-    method = method,
-    determiner = determiner,
-    order = order,
-    family = family,
-    sub.family = sub.family,
-    genus = genus,
-    taxon = taxon,
-    log.number = log.number,
-    Bradley = Bradley,
-    common.name = common.name,
-    gender = gender
-    )
-
+  my.lists <- datum.attributes
   my.working.project <- rstudioapi::getActiveProject()
 
   my.working.directory <- fs::path(getwd())
@@ -74,7 +28,11 @@ new.record <- function(index.reference,
   dir.create(images.dir)
 
   # Path to inst
- data.model.path <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
+  if(is.null(data.model.path)) {
+    data.model.path <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
+
+  }
+
   # Now create the template file
  my.template <- readr::read_file(data.model.path) %>%   as.character()
 
