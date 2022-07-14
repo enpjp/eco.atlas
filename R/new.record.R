351073dc639fe33c7bb21eaaa11161eb8688708b
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2022-07-14 20:50:19 +0100

    Update new.record.R

--- R/new.record.R
+++ R/new.record.R
@@ -75,14 +75,47 @@ new.record <- function(
                                    index.reference, ext = "Rmd")
          readr::write_file(my.rendered.output, rendered.path)
    }
-  # usethis::with_project( my.working.project,
-  #                        usethis::use_template(data_model,
-  #                                              save_as = fs::path(my.working.directory,
-  #                                                                 index.reference, ext = "Rmd"),
-  #                                              index.reference = index.reference
-  #                        )
-  #                        # End use with project
-  # )
+
+
+   if(overwrite) {
+
+
+
+
+     dir.create(index.reference)
+
+     # Now create an images directory
+     images.dir <- fs::path(my.working.directory,
+                            index.reference,
+                            "images",
+                            "dissection")
+     fs::dir_create(images.dir, recurse = TRUE)
+     # create a placeholder for git
+     readr::write_file( "Images here." ,
+                        fs::path( images.dir,
+                                  "image_placeholder",
+                                  ext = "txt"
+
+                        ) )
+
+
+     # Path to inst
+     if(is.null(data.model.path)) {
+       data.model.path <- system.file("model_template","data_model.Rmd",package = "eco.atlas")
+
+     }
+
+     # Now create the template file
+     my.template <- readr::read_file(data.model.path) %>%   as.character()
+
+     my.rendered.output <-
+       whisker::whisker.render(my.template, data = my.lists, strict = FALSE)
+     # Write the file
+     rendered.path <- fs::path(my.working.directory,
+                               index.reference,
+                               index.reference, ext = "Rmd")
+     readr::write_file(my.rendered.output, rendered.path)
+   }
 
 
 }
