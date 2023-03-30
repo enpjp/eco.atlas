6b8f66b5273a0f83ace918e55b3d274140cfcb9b
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2023-03-30 19:54:15 +0100

    Create taxonomy.parent.R

---
+++ R/taxonomy.parent.R
@@ -0,0 +1,37 @@
+
+taxonomy.parent <- function(taxon_key) {
+
+ # NAMES <- UKSI.names
+  TAXA <- UKSI.taxa
+
+  cols.to.use <- c("TAXON_VERSION_KEY",
+                   "RANK",
+                   "TAXON_NAME",
+                   "ORGANISM_KEY",
+                   "PARENT_KEY")
+
+  UKSI.taxa.cols <- TAXA[,cols.to.use]
+  parent.out <- UKSI.taxa.cols[
+    UKSI.taxa.cols$ORGANISM_KEY == taxon_key,] %>% head(n = 1)
+
+  out.cols <- data.frame (parent.out$TAXON_NAME )
+ colnames(out.cols)[1] <- parent.out$RANK
+
+# Now add a while loop to climb all the way up the taxonomy tree.
+ # Add a counter to stop after 20 steps anyway.
+
+ while(!is.na(parent.out$PARENT_KEY)) {
+
+   UKSI.taxa.cols <- TAXA[,cols.to.use]
+   parent.out <- UKSI.taxa.cols[
+     UKSI.taxa.cols$ORGANISM_KEY == parent.out$PARENT_KEY,]
+
+   out.cols.recursive <- data.frame (parent.out$TAXON_NAME )
+   colnames(out.cols.recursive)[1] <- parent.out$RANK
+
+   out.cols <- cbind(out.cols, out.cols.recursive)
+ }
+
+
+  return(out.cols)
+}
