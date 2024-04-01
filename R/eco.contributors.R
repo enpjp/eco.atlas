a30d8bf1cd573167112749289d8ea715b1efe39a
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2024-04-01 19:59:10 +0100

    update

---
+++ R/eco.contributors.R
@@ -0,0 +1,39 @@
+# Get list of contributors
+
+#' Contributors To Report
+#'
+#' @param my.data.ss Search data for names of contributors
+#'
+#' @return List of contributors and clipboard text.
+#' @export eco.contributors
+#'@importFrom magrittr "%>%"
+#'
+eco.contributors <- function(my.data.ss) {
+
+  thanks.stack <- utils::stack( my.data.ss[,c("recorder.name","determiner")]) %>% unique()
+
+  thanks.stack$values <- gsub("NULL",NA,thanks.stack$values)
+
+   thanks.expanded <- thanks.stack %>% tidyr::separate_longer_delim("values", sep = "&|;|,")
+
+   thanks.expanded$values <- thanks.expanded$values %>% stringr::str_trim()
+
+  thanks <-  thanks.expanded %>% unique()  %>% tidyr::drop_na()
+
+  thanks.recorders <- thanks %>% filter("ind" == "recorder.name")
+  thanks.determiners <- thanks %>% filter("ind" == "determiner")
+
+  # Now make a thanks string
+  recorder.text <- knitr::combine_words(thanks.recorders$values)
+  determiner.text <-  knitr::combine_words(thanks.determiners$values)
+
+  thanks.text <- paste("Thanks to ",recorder.text,
+                             " for providing specimens. The assistance with determinations by ",
+                             determiner.text,
+                             " is gratefully acknowledged.",
+                       sep = ""
+                        )
+  clipr::write_clip(thanks.text)
+
+  return(thanks)
+}
