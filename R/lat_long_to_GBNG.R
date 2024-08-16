416c75234663b48e1e44908e520711a9e613e1af
Paul Palmer <p.j.palmer@tech-trends.co.uk>
2024-08-16 20:28:46 +0100

    Update

---
+++ R/lat_long_to_GBNG.R
@@ -0,0 +1,35 @@
+
+
+#' Lattitude Longitude To UKNG
+#'
+#' @param lattitude as it says
+#' @param longitude as it says
+#'
+#' @return GBNG
+#' @export lat_long_to_GBNG
+#'
+#' @examples
+lat_long_to_GBNG <- function(lattitude, longitude){
+
+  coords <- NULL
+  coords$x <- longitude
+  coords$y <- lattitude
+
+  df.coords <- as.data.frame(coords)
+  df.coords$x %>% as.numeric
+  df.coords$y %>% as.numeric
+
+
+  sp.coords.sf  <- sf::st_as_sf(x = df.coords,
+                 coords = c("x", "y"),
+                 crs = sf::st_crs(4326))
+
+  #sp.coords.sf <- sf::st_as_sf(sp.coords)
+
+  sp.coords.sf.ng  <-  sf::st_transform(sp.coords.sf, crs = sf::st_crs(27700))
+
+  GBNG.out <- eco.atlas::sf_to_UKNGR_Letter(sp.coords.sf.ng)
+
+}
+
+
